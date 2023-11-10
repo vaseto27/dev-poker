b1ecba63815d1eeb2107071d3b98f246cbbd68fc
Vasil Evgeniev Aleksandrov <vvvaleksandrovv@gmail.com>
2023-11-10 16:57:17 +0200

    added player req

--- index.js
+++ index.js
@@ -2,17 +2,33 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const PokerGame = require('./backend/pokerGame');
+const Player = require('./backend/player')
 
 
 const app = express();
+let game;
 
 const startGame = () => {
-    const game = new PokerGame();
+     game = new PokerGame();
 }
+
+startGame();
 
 const DEFAULT_PORT = 3000;
 
 app.use(bodyParser.json());
+
+app.use('/api/start-game', (req, res) => {
+    startGame();
+    res.send('Game started')
+})
+
+app.post('/api/add-player', (req, res) => {
+    const {name, balance} = req.body;
+    const newPlayer = new Player(name, balance);
+    game.addPlayer(newPlayer);
+    res.status(200).json({data: newPlayer ,message: 'Player added succesfully'})
+})
 
 
 app.listen(DEFAULT_PORT, () => {
